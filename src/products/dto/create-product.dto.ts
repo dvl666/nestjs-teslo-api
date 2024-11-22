6c772ee971e74cc4d018c9a8a9e5b60cdb5e044b
dvl666 <kamagaspar@gmail.com>
2024-11-22 01:24:01 -0300

    update create-products.dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,36 @@
+import { IsArray, IsIn, IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Min } from "class-validator"
 
 
-export class CreateProductDto {}
+export class CreateProductDto {
+
+    @IsString()
+    @Min(1)
+    @IsNotEmpty()
+    title: string
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number
+
+    @IsString()
+    @IsOptional()
+    description?: string
+
+    @IsString()
+    @IsOptional()
+    slug?: string
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock: number
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[]
+
+    @IsIn(['men', 'woman', 'unisex', 'kids'])
+    gender: string
+
+}
