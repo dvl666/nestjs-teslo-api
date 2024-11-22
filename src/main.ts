ecbc67d91eea511f3112aa4a8a03cce83734dec2
dvl666 <kamagaspar@gmail.com>
2024-11-22 01:13:21 -0300

    add api prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // -> se agrega un prefijo a todas las rutas
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
