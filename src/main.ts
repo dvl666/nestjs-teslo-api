3b2d3f924cf02f8c819920e5c9a6335845399fae
dvl666 <kamagaspar@gmail.com>
2024-12-01 23:39:43 -0300

    comments

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,17 @@ async function bootstrap() {
   app.setGlobalPrefix('api'); // -> se agrega un prefijo a todas las rutas
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      // whitelist: true,
+      // forbidNonWhitelisted: true,
       /**
        * transform: true -> Con esto se habilita la transformacion de los datos, por ejemplo si se recibe un string
        * enableImplicitConversion: true -> Con esto se habilita la conversion implicita de los tipos de datos, por ejemplo
        * si se espera un numero y se recibe un string 
        */
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      }
+      // transform: true,
+      // transformOptions: {
+      //   enableImplicitConversion: true,
+      // }
     })
   ); // -> se agregan validaciones a todas las rutas
   await app.listen(process.env.PORT ?? 3000);
