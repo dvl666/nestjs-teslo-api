91153ec1d215dece2bf7b06691a8b83283245a9f
dvl666 <kamagaspar@gmail.com>
2024-12-04 22:29:51 -0300

    add images on product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -36,6 +36,11 @@ export class CreateProductDto {
     @IsString({ each: true })
     @IsArray()
     @IsOptional()
-    tags: string[]
+    tags?: string[]
+
+    @IsString({ each: true })
+    @IsArray()
+    @IsOptional()
+    images?: string[]
 
 }
