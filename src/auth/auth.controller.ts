94ec84c7e565074475a96944217f29de7f286ef6
dvl666 <kamagaspar@gmail.com>
2024-12-08 17:51:52 -0300

    add private to test jwt and custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +20,28 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+
+  @Get('private')
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[]
+  ) {
+    // console.log('Hola soy el usuario en el request del authController', request.user)
+    // console.log('Hola soy el request', request)
+    console.log(user)
+    console.log(rawHeaders)
+    return {
+      ok: true,
+      message: 'You have access to this route',
+      user,
+      userEmail
+    }
+  
   }
 
 }
