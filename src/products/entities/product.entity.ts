39ae3c91a503ce206ecfa8566d5b698ac54284f7
dvl666 <kamagaspar@gmail.com>
2024-12-09 20:23:49 -0300

    add user => produc relation

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
+import { User } from "src/auth/entities/user.entity";
 
 @Entity()
 export class Product {
@@ -40,9 +41,16 @@ export class Product {
     @OneToMany(
         () => ProductImage,
         ( image ) => image.product,
-        { cascade: true }
+        { cascade: true, eager: true },
     )
     images: ProductImage[]
+
+    @ManyToOne(
+        () => User,
+        ( user ) => user.product,
+        { onDelete: 'CASCADE' }
+    )
+    user: User
 
     @BeforeInsert()
     checkSlugInsert() {
