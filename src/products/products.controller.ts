4627f58771c3378da3730129d72b190353824335
dvl666 <kamagaspar@gmail.com>
2024-12-09 20:24:20 -0300

    update product.controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,14 +3,25 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { User } from 'src/auth/entities/user.entity';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
+import { RawHeaders } from 'src/auth/decorators/raw-headers.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Auth(ValidRoles.admin, ValidRoles.user)
+  create(
+    @GetUser() user: User,
+    @RawHeaders() rawHeaders: any,
+    @Body() createProductDto: CreateProductDto
+  ) {
+    console.log(rawHeaders);
+    return this.productsService.create(createProductDto, user);
   }
 
   @Get()
@@ -24,11 +35,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(
+    @GetUser() user: User,
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto
   ) {
-    return this.productsService.update(id, updateProductDto);
+    return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
