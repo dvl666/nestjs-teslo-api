c296180c36af05b9ca5ef3eebc9dea453b7660b3
dvl666 <kamagaspar@gmail.com>
2024-12-09 20:25:00 -0300

    update seed.controller

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
@@ -8,6 +10,7 @@ export class SeedController {
   ) {}
 
   @Get()
+  // @Auth( ValidRoles.user )
   seed() {
     return this.seedService.seed();
   }
